8d3fc02fef23e01aa6f7cd85d333339780af55cf
Aanya Jain <jainaanya2000@gmail.com>
2020-07-11 23:25:45 +0530

    added components

---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+export default class EditExercise extends Component{
+    render(){
+        return(
+            <div>
+            <p>You are on the EditExercises exercises List page</p>
+            </div>
+        )
+    }
+}
+
