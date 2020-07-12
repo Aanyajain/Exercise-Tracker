e853dcdd7fbe5e41ff580197d9a3889f87b1d6e6
Aanya Jain <jainaanya2000@gmail.com>
2020-07-12 11:48:23 +0530

    added exercise and user comp

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 
 export default class EditExercise extends Component{
     render(){
-        return(
+         return(
             <div>
             <p>You are on the EditExercises exercises List page</p>
             </div>
